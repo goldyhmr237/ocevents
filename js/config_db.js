ce918db09c7181d8633c326239bf05aa8fdfe020
chh_rahul <softweaver.tech@gmail.com>
2015-11-03 14:41:36 +0530

    37

--- js/config_db.js
+++ js/config_db.js
@@ -1,5 +1,3 @@
-//navigator.openDatabase = window.openDatabase = DroidDB_openDatabase;
-//window.droiddb = new DroidDB();
 var db = window.openDatabase('OCEVENTS', '1.0', 'OCEVENTS', 2 * 1024 * 1024);
 var server_url = 'http://oceventmanager.com/';
 var static_event_id = '100041';
