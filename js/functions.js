593158a440b80b547ad54cba69d54960c684238c
chh_rahul <softweaver.tech@gmail.com>
2016-04-06 15:25:29 +0530

    217

--- js/functions.js
+++ js/functions.js
@@ -1137,33 +1137,31 @@ var login = function() {
 
                             //var img_src = 'http://weknowyourdreams.com/images/love/love-09.jpg';
                             var image_name = getFileNameFromPath(img_src);
-                             alert(img_src);
-                              alert(image_name);
+                            // alert(img_src);
+                              //alert(image_name);
                             var STR = localStorage.url + "api/index.php/main/base64Image?XDEBUG_SESSION_START=PHPSTORM&image=" + img_src;
 
-                            alert('hi')
+                            //alert('hi')
                             $.ajax({
                                 url: STR,
                                 dataType: "html",
                                 success: function(DtatURL) {
 
-                                    //alert(DtatURL);  
-                                    //adb logcat *:E		 
-                                     //alert(obj.data.image.image_src);
+                                   
                                     b.download_file(DtatURL, DIR_Name + '/', getFileNameFromPath(obj.data.image.image_src), function(theFile) {
 
                                         var ImgFullUrl = '';
                                         ImgFullUrl = theFile.toURI();
-                                        // alert(ImgFullUrl);
+                                        
                                         db.transaction(function(tx) {
                                             tx.executeSql('CREATE TABLE IF NOT EXISTS OCEVENTS_user (id integer primary key autoincrement,team,position,fb_user_id,fb_email,birthday_date,website, user_id, email, first_name, last_name,mobile, image_src, is_user_image, created,gender,player_code)');
                                             tx.executeSql("delete from OCEVENTS_user");
                                             tx.executeSql('INSERT INTO OCEVENTS_user (team,position,fb_user_id,fb_email,birthday_date,website,user_id,email,first_name,last_name,mobile,image_src,is_user_image,created,gender,player_code) VALUES ("' + obj.data.team + '","' + obj.data.position + '","' + obj.data.fb_user_id + '","' + obj.data.fb_email + '","' + obj.data.birthday_date + '","' + obj.data.website + '","' + obj.data.id + '","' + obj.data.email + '","' + obj.data.first_name + '","' + obj.data.last_name + '","' + obj.data.mobile + '","' + ImgFullUrl + '","' + obj.data.image.is_user_image + '","' + obj.data.created + '","' + obj.data.gender + '","' + obj.data.player_code + '")');
                                             localStorage.user_id = obj.data.id;
                                             localStorage.event_id = obj.data.event_id;
-                                            //alert('started')
+                                            
                                             getLoggedInUser();                                             
-                                           // login_process();
+                                           
                                         });
                                     });
 
