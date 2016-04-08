b9d45dcb3c1e8ca8b1985fcb5cbb14dd13cf5130
chh_rahul <softweaver.tech@gmail.com>
2016-04-08 12:42:35 +0530

    234

--- js/functions.js
+++ js/functions.js
@@ -4481,11 +4481,11 @@ function loadnotes()
                   //comment_video = '<div style=background-image:url("'+ localStorage.url+'resources/files/videos/'+val.thumb_filename+'") class="video-item"><div class="video-wrapper"><div class="video-container"><div class="future-video video" style="display:block;" onclick=playvideo("' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '");><img src="img/bigplay.png" class="video_comment" /></div></div></div></div>';
                   //comment_video = '<div class="video-item"><div class="video-wrapper"><div class="video-container"> <video class="future-video video" controls><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '" webkit-playsinline width="480" height="320" type="video/mp4"></video></div></div></div></div>'; 
                   
-                  comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video js-video_'+val.instance_id+'"><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '">Your browser does not support HTML5 video.</video><canvas class="canvas js-canvas_'+val.instance_id+'"></canvas><div class="video-timeline js-timeline_'+val.instance_id+'"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>'; 
+                  comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video jsv_'+val.instance_id+' js-video"><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '">Your browser does not support HTML5 video.</video><canvas class="canvas jsc_'+val.instance_id+' js-canvas"></canvas><div class="video-timeline jst_'+val.instance_id+' js-timeline"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>'; 
                   var canvasVideo = new CanvasVideoPlayer({
-                  			videoSelector: '.js-video_'+val.instance_id,
-                  			canvasSelector: '.js-canvas_'+val.instance_id,
-                  			timelineSelector: '.js-timeline_'+val.instance_id,
+                  			videoSelector: '.jsv_'+val.instance_id,
+                  			canvasSelector: '.jsc_'+val.instance_id,
+                  			timelineSelector: '.jst_'+val.instance_id,
                   			audio: true
                     });
               }
@@ -5820,12 +5820,12 @@ function showcomments()
                   //comment_video = '<div style=background-image:url("'+ localStorage.url+'resources/files/videos/'+val.thumb_filename+'") class="video-item"><div class="video-wrapper"><div class="video-container"><div class="future-video video" style="display:block;" onclick=playvideo("' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '");><img src="img/bigplay.png" class="video_comment" /></div></div></div></div>';  
                   //comment_video = '<div class="video-item"><div class="video-wrapper"><div class="video-container"> <video class="future-video video" controls><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '" webkit-playsinline width="480" height="320" type="video/mp4"></video></div></div></div></div>'; 
                   
-                  comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video js-video_'+val.instance_id+'"><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '">Your browser does not support HTML5 video.</video><canvas class="canvas js-canvas_'+val.instance_id+'"></canvas><div class="video-timeline js-timeline_'+val.instance_id+'"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>';  
+                  comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video jsv_'+val.instance_id+' js-video"><source src="' + localStorage.url+ 'resources/files/videos/' + val.video_filename + '">Your browser does not support HTML5 video.</video><canvas class="canvas jsc_'+val.instance_id+' js-canvas"></canvas><div class="video-timeline jst_'+val.instance_id+' js-timeline"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>';  
                  // alert(comment_video)
                  var canvasVideo = new CanvasVideoPlayer({
-                  			videoSelector: '.js-video_'+val.instance_id,
-                  			canvasSelector: '.js-canvas_'+val.instance_id,
-                  			timelineSelector: '.js-timeline_'+val.instance_id,
+                  			videoSelector: '.jsv_'+val.instance_id,
+                  			canvasSelector: '.jsc_'+val.instance_id,
+                  			timelineSelector: '.jst_'+val.instance_id,
                   			audio: true
                     });
               }
