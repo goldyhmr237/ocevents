4f37f8f83218c46eb1b4f4314f0986c5724d9e1d
chh_rahul <softweaver.tech@gmail.com>
2016-04-08 16:41:54 +0530

    259

--- js/functions.js
+++ js/functions.js
@@ -1275,7 +1275,7 @@ function loadgamification() {
                       
                 		}); */
                      //alert(localStorage.url+ 'resources/files/videos/' + results.rows.item(0).banner_video)
-                     comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video js-video"><source src="' + localStorage.url+ 'resources/files/videos/' + results.rows.item(0).banner_video + '">Your browser does not support HTML5 video.</video><canvas class="canvas js-canvas"></canvas><div class="video-timeline js-timeline"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>';
+                     var comment_video = '<div class="video-item"><div class="video-wrapper js-video-wrapper"><div class="video-responsive"><video  class="video future-video js-video"><source src="' + localStorage.url+ 'resources/files/videos/' + results.rows.item(0).banner_video + '">Your browser does not support HTML5 video.</video><canvas class="canvas js-canvas"></canvas><div class="video-timeline js-timeline"><div class="video-timeline-passed js-timeline-passed"></div></div></div></div></div>';
                   var canvasVideo = new CanvasVideoPlayer({
                   			videoSelector: '.js-video',
                 			canvasSelector: '.js-canvas',
